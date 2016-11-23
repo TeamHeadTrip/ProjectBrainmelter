da7cd5a14126ba0e303de0f523027f6b51a67f88
Daniel Lewis <thecontrarian42@gmail.com>
2016-11-22 21:57:12 -0600

    stuff

---
+++ Assets/scale.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scale : MonoBehaviour {
+
+	Vector3 startScale;
+	float startDist;
+	float lastDist;
+	
+	void Start() {
+		startScale = transform.localScale;
+		startDist = Vector3.Distance(transform.position, Camera.main.transform.position); //transform.position.z - Camera.main.transform.position.z;
+		lastDist = startDist;
+	}
+	
+	void Update() {
+		
+		var dist = Vector3.Distance(transform.position, Camera.main.transform.position);
+		if (dist != lastDist) {
+			transform.localScale = startScale / startDist * dist;
+			lastDist = dist;
+		}
+	}
+}
